a9005f0a449037a58fbd1772bf2e51f62de7b0bd
Brian Xu <44307081+BlazeBQX@users.noreply.github.com>
2024-02-21 14:19:29 -0600

    Added Timeline, partial fixed slideshow
    Need to fix slideshow, and timeline needs to have dropdowns next

--- .idea/homepage.js
+++ .idea/homepage.js
@@ -1,18 +1,25 @@
-var slideIndex = 0;
+var slideIndex = 1;
 carousel();
 
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function plusSlides(n) {
+    carousel(slideIndex += n);
   }
-  
-  if (slideIndex > x.length) {slideIndex = 1
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 5000); 
-  slideIndex++;
-  }// Change image every 5 seconds
-
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+function carousel(n) {
+    let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
