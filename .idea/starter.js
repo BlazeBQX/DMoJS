42c1cac7a453234dcdcff1023706e173bc0c1b5b
Brian Xu <44307081+BlazeBQX@users.noreply.github.com>
2024-03-06 12:20:48 -0600

    Added arrow function on judo
    Need to change rest of page to match the function

--- .idea/starter.js
+++ .idea/starter.js
@@ -1,36 +1,17 @@
-const express = require('express');
-const app = express();
 
+function contentDrop(){
+  
+   
+    let main = document.getElementsByClassName("content");
+    let arrow = document.getElementsByClassName("arrow")
+    arrow[0].style.display="none";
+    let image = document.getElementById("image");
+    image.style.width="325px";
+    image.style.height="400px";
+    setTimeout(1000);
+    let expanded = document.getElementsByClassName("expanded-content");
+    expanded[0].style.display="block";
 
-
-
-
-
-
-
-
-//code for a user to upload an image, might be needed if we do manual updates, then Colton can add stuff by himself.
-
-
-/*const path = require('path')
-
-const multer = require('multer');
-const storage = multer.diskStorage({
-    destination: (req,file,cb)=> {
-        cb(null, 'assets/images')
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
-
-app.get("/upload",(req,res)=> {
-    res.render("upload");
-});
-
-const upload = multer({storage: storage})
-app.post("/upload",upload.single("image"),(req,res)=>{res.send("Image Uploaded")})
-*/ 
-
-app.listen(3001);
-console.log("Port: 3001")
+        
+      
+}
